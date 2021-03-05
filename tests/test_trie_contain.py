824d720ee994219e413998e1fa0be8ca2d4f285f
mitchiezhao <mitchie.zhao12@gmail.com>
2021-03-05 10:43:01 -0800

    Add test for one more branch

--- tests/test_trie_contain.py
+++ tests/test_trie_contain.py
@@ -28,6 +28,28 @@ def test_contain_missing_word():
     assert "c" in trie.root.children["a"].children
     assert not trie.root.children["a"].children["c"].is_complete_word
 
+def test_contain_missing_word2():
+    trie = Trie()
+    trie.root.children = {
+        'a': TrieNode(is_complete_word=False, children={
+            'c': TrieNode(is_complete_word=False, children={
+            't': TrieNode(is_complete_word=True, children={})
+            }),
+        'd': TrieNode(is_complete_word=True, children={})
+        })
+    }
+    #  root
+    #   |
+    #   a
+    #  / \
+    # c  d (complete word)
+    # |
+    # t (complete word)
+    
+    contain_word = trie.contain("bag")
+
+    assert contain_word == False
+
 def test_contain_existing_word():
 
     trie = Trie()
