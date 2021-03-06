485b076a5e5e5c5859fcafd05c227483e3ce1c82
jordanlau123 <jordannlau@gmail.com>
2021-03-05 16:25:44 -0800

    added docstring to test functions

--- tests/test_trie_add.py
+++ tests/test_trie_add.py
@@ -7,25 +7,40 @@ def test_version():
     assert __version__ == '0.1.0'
 
 def test_add():
+    """
+    this test function tests if the add function will return True when adding a new word
+    """
     trie = Trie()
     assert ((trie.add("hello")) == True)
 
 def test_add_existing_word():
+    """
+    this test function tests if the add function will return False when adding an existing word
+    """
     trie = Trie()
     trie.add("hello")
     assert ((trie.add("hello")) == False)
 
 def test_add_word_is_string():
+    """
+    this test function tests if the add function will give a TypeError when inserting something other than a string 
+    """
      trie = Trie()  
      with pytest.raises(TypeError):
          trie.add(23)
 
 def test_add_empty_string():
+    """
+    this test function tests if the add function will give a ValueError when inserting an empty string
+    """
      trie = Trie()       
      with pytest.raises(ValueError):
          trie.add("")
 
 def test_add_sucessfully_add_word():
+    """
+    this test function tests if the add function will correctly add the word 
+    """
     trie = Trie()
 
     #  root
@@ -41,6 +56,9 @@ def test_add_sucessfully_add_word():
     assert trie.root.children["h"].children["e"].is_complete_word
 
 def test_add_sucessfully_creates_new_node():
+    """
+    this test function tests if the add function will correctly create a new node when adding a word 
+    """
     trie = Trie()
 
     #  root
@@ -59,6 +77,9 @@ def test_add_sucessfully_creates_new_node():
     assert trie.root.children["h"].children["e"].is_complete_word
 
 def test_add_to_existing_node():
+    """
+    this test function tests if the add function will correctly add a word to an existing child node
+    """
     trie = Trie()
     
     trie.add("he")
